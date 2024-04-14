4022255d7c40de353d70aa3ae55c5ee3af872f67
Brilliant Sam <brilliantsam12345@gmail.com>
2024-04-14 12:37:21 -0400

    v1.4

--- Researcher.java
+++ Researcher.java
@@ -8,12 +8,22 @@ public class Researcher{
     private double multiplier;
     private ArrayList<Integer> industry;
     private int level;
+    private int[] override;
     
     public Researcher(String upg, double none, double init, double mult, ArrayList<Integer> ind){
         this.none = none;
         upgrade = upg;
         initial = init;
         multiplier = mult;
+        industry = ind;
+        level = 0;
+        override = new int[] {};
+    }
+    
+    public Researcher(String upg, double none, int[] ovrd, ArrayList<Integer> ind){
+        this.none = none;
+        upgrade = upg;
+        override = ovrd;
         industry = ind;
         level = 0;
     }
@@ -23,27 +33,40 @@ public class Researcher{
     }
     
     public double getBoost(){
-        if(upgrade.equals("prod")){
-            if(level > 0){
-                return initial * (int)(Math.pow(multiplier, level - 1));
+        if(override.length == 0){
+            if(upgrade.equals("prod")){
+                if(level > 0){
+                    return initial * (int)(Math.pow(multiplier, level - 1));
+                } else{
+                    return none;
+                }
+            } else if(upgrade.equals("crit")){
+                if(level > 0){
+                    return none * initial * (int)(Math.pow(multiplier, level - 1));
+                } else{
+                    return none;
+                }
+            } else if(upgrade.equals("luck")){ // EXCEPTION: INIT IS ADDED TO EVERY LEVEL, MULT IS BONUS BOOST, SUCH AS IN MINIS
+                double bonus = 0;
+                for(int i = level; i > 1; i--){
+                    bonus += ((i - 1) * multiplier);
+                }
+                //System.out.println(bonus);
+                return none + initial * level + bonus;
             } else{
-                return none;
+                return -1;
             }
-        } else if(upgrade.equals("crit")){
-            if(level > 0){
-                return none * initial * (int)(Math.pow(multiplier, level - 1));
-            } else{
-                return none;
-            }
-        } else if(upgrade.equals("luck")){ // EXCEPTION: INIT IS ADDED TO EVERY LEVEL, MULT IS BONUS BOOST, SUCH AS IN MINIS
-            double bonus = 0;
-            for(int i = level; i > 1; i--){
-                bonus += ((i - 1) * multiplier);
-            }
-            //System.out.println(bonus);
-            return none + initial * level + bonus;
         } else{
-            return -1;
+            if(level < override.length){
+                if(upgrade.equals("luck")){
+                    return override[level] + none;
+                } else{
+                    return override[level] * none;
+                }
+            } else{
+                System.out.println("ERROR: ONE OR MORE RARE RESEARCHERS ARE ABOVE MAX LEVEL");
+                return -1;
+            }
         }
     }
     
